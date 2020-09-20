e32a63106ea93d9d23ebdd8bbce289ac4911e72a
Darcy Linde <lindedarcy@gmail.com>
2020-09-20 18:17:18 -0400

    created Checklist component

---
+++ src/Checklist.svelte
@@ -0,0 +1,19 @@
+<script>
+  let tasks = ["read"];
+  $: count = tasks.length;
+  let newTask = "code";
+
+  function addTask() {
+    tasks = [...tasks, newTask];
+    newTask = "";
+  }
+</script>
+
+<p>You have {count} {count === 1 ? 'item' : 'items'} on your list</p>
+<input bind:value={newTask} />
+<button on:click={() => addTask()}>Insert New Task</button>
+<ul>
+  {#each tasks as task}
+    <li>{task}</li>
+  {/each}
+</ul>
