928a0c96aeff12b696f7308fff0a78d04cae241f
Darcy Linde <lindedarcy@gmail.com>
2020-09-22 14:11:16 -0400

    installed firebase and added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1PpXLUfWxv1OFOXUTbfepP3m6wB-72hc",
+  authDomain: "schedule-app-59f48.firebaseapp.com",
+  databaseURL: "https://schedule-app-59f48.firebaseio.com",
+  projectId: "schedule-app-59f48",
+  storageBucket: "schedule-app-59f48.appspot.com",
+  messagingSenderId: "873166960530",
+  appId: "1:873166960530:web:3297459cb7db784962334b",
+  measurementId: "G-KGREM464WW",
+};
+
+firebase.initializeApp(firebaseConfig);
